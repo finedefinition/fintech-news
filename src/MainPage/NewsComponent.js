c015457a8fd61d0c211648f004743cb7f7746d16
Sergii Bezrukov <73116558+finedefinition@users.noreply.github.com>
2024-01-05 12:50:53 +0000

    Implement Title search

--- src/MainPage/NewsComponent.js
+++ src/MainPage/NewsComponent.js
@@ -4,39 +4,27 @@ import 'react-datepicker/dist/react-datepicker.css'; // Import the CSS for the d
 import './NewsComponent.css';
 
 const NewsComponent = () => {
-    const [newsData, setNewsData] = useState([]);
     const [startDate, setStartDate] = useState(new Date());
     const [endDate, setEndDate] = useState(new Date());
     const [selectedTicket, setSelectedTicket] = useState('');
+    const [localNewsData, setLocalNewsData] = useState([]);
     const [ticketsList, setTicketsList] = useState([
         'AAPL', 'MSFT', 'GOOGL', 'AMZN', 'FB', 'TSLA', 'BRK.A', 'JPM',
         'V', 'JNJ', 'WMT', 'PG', 'UNH', 'MA', 'NVDA', 'HD', 'BAC',
         'PFE', 'DIS', 'VZ'
     ]);
-    const [currentPage, setCurrentPage] = useState(1);
-    const [itemsPerPage] = useState(5); // You can set this to any number you want for items per page
-
-    // Derived state for pagination
-    const indexOfLastItem = currentPage * itemsPerPage;
-    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-    const currentItems = newsData.slice(indexOfFirstItem, indexOfLastItem);
-
-    // Function to change page
-    const paginate = (pageNumber) => setCurrentPage(pageNumber);
-
 
     useEffect(() => {
         const fetchData = async () => {
-            try {
-                if (selectedTicket) {
-                    const response = await fetch(
-                        `https://api.polygon.io/v2/reference/news?ticker=${selectedTicket}&apiKey=KPcpXIk3tqCf5HBomDdRTQR2WTTFUDXy&published_utc.gt=${startDate.toISOString()}&published_utc.lt=${endDate.toISOString()}`
-                    );
+            if (selectedTicket) {
+                try {
+                    const localUrl = `http://localhost:8080/news?ticker=${selectedTicket}&firstDate=${startDate.toISOString()}&lastDate=${endDate.toISOString()}`;
+                    const response = await fetch(localUrl);
                     const data = await response.json();
-                    setNewsData(data.results);
+                    setLocalNewsData(data || []);
+                } catch (error) {
+                    console.error('Error fetching local news data:', error);
                 }
-            } catch (error) {
-                console.error('Error fetching news data:', error);
             }
         };
 
@@ -47,74 +35,70 @@ const NewsComponent = () => {
         setSelectedTicket(event.target.value);
     };
 
-    const pageNumbers = [];
-    for (let i = 1; i <= Math.ceil(newsData.length / itemsPerPage); i++) {
-        pageNumbers.push(i);
-    }
+    console.log('Local News Data:', localNewsData);
 
     return (
         <div className="news-container">
-            <div className="ticket-selector">
-                <label htmlFor="ticket-select">Choose a ticker:</label>
-                <select id="ticket-select" value={selectedTicket} onChange={handleTicketChange}>
-                    <option value="">Select a ticker</option>
-                    {ticketsList.map((ticket) => (
-                        <option key={ticket} value={ticket}>{ticket}</option>
-                    ))}
-                </select>
-            </div>
-            <div className="date-picker-container">
-                <label htmlFor="start-date-picker">Pick up the date:</label>
-                <DatePicker
-                    selected={startDate}
-                    onChange={(date) => setStartDate(date)}
-                    selectsStart
-                    startDate={startDate}
-                    endDate={endDate}
-                    dateFormat="MM/dd/yyyy"
-                />
-                <DatePicker
-                    selected={endDate}
-                    onChange={(date) => setEndDate(date)}
-                    selectsEnd
-                    startDate={startDate}
-                    endDate={endDate}
-                    minDate={startDate}
-                    dateFormat="MM/dd/yyyy"
-                />
+            <div className="selection-container">
+                <div className="ticket-selector">
+                    <label htmlFor="ticket-select">Choose a ticker:</label>
+                    <select id="ticket-select" value={selectedTicket} onChange={handleTicketChange}>
+                        <option value="">Select a ticker</option>
+                        {ticketsList.map((ticket) => (
+                            <option key={ticket} value={ticket}>{ticket}</option>
+                        ))}
+                    </select>
+                </div>
+                <div className="date-picker-container">
+                    <label htmlFor="start-date-picker">Pick up the date:</label>
+                    <DatePicker
+                        selected={startDate}
+                        onChange={(date) => setStartDate(date)}
+                        selectsStart
+                        startDate={startDate}
+                        endDate={endDate}
+                        dateFormat="MM/dd/yyyy"
+                    />
+                    <DatePicker
+                        selected={endDate}
+                        onChange={(date) => setEndDate(date)}
+                        selectsEnd
+                        startDate={startDate}
+                        endDate={endDate}
+                        minDate={startDate}
+                        dateFormat="MM/dd/yyyy"
+                    />
+                </div>
             </div>
             <div className="news-items">
-                {currentItems.map((news) => (
-                    <div key={news.id} className="news-item">
-                        <img src={news.publisher.logo_url} alt={news.publisher.name} className="news-logo"/>
-                        <div className="news-content">
-                            <h3 className="news-title">{news.title}</h3>
-                            <p className="news-author">
-                                <strong>Author:</strong> {news.author}
-                            </p>
-                            <p className="news-published">
-                                <strong>Published UTC:</strong> {new Date(news.published_utc).toLocaleString()}
-                            </p>
-                            <p className="news-description">
-                                {news.description}
-                            </p>
+                <h2>Title search</h2>
+                {localNewsData.length > 0 ? (
+                    localNewsData.map((news, index) => (
+                        <div key={`local-${index}`} className="news-item">
+                            <img src={news.publisher.logo_url} alt={news.publisher.name} className="news-logo"/>
+                            <div className="news-content">
+                                <h3 className="news-title">{news.title}</h3>
+                                <p className="news-author">
+                                    <strong>Author:</strong> {news.author}
+                                </p>
+                                <p className="news-published">
+                                    <strong>Published UTC:</strong> {new Date(news.published_utc).toLocaleString()}
+                                </p>
+                                <p className="news-description">
+                                    {news.description}
+                                </p>
+                            </div>
+                            <a href={news.article_url} className="news-read-more" target="_blank" rel="noopener noreferrer">
+                                Read More
+                            </a>
                         </div>
-                        <a href={news.article_url} className="news-read-more" target="_blank" rel="noopener noreferrer">
-                            Read More
-                        </a>
-                    </div>
-                ))}
-            </div>
-            <div className="pagination">
-                {pageNumbers.map(number => (
-                    <button key={number} onClick={() => paginate(number)}
-                            className={currentPage === number ? 'active' : ''}>
-                        {number}
-                    </button>
-                ))}
+                    ))
+                ) : (
+                    <p className="no-news-message">No local news to display.</p>
+                )}
             </div>
         </div>
     );
-}
+};
 
 export default NewsComponent;
